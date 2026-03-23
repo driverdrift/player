93dbad3bba5775645cbf95653c93a603d35d776b
driverdrift <kxktb@outlook.com>
2026-03-23 19:07:44 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+apt update
+# 安装 ocr
+apt install -y tesseract-ocr
+# 安装图片预处理工具
+apt install -y imagemagick
+
+
+curl -b cookies.txt "https://vidhub4.cc/vodsearch/-------------.html?wd=robot"
+
+
+# 1. 安装 virtualenv（如果没装）
+sudo apt update
+sudo apt install python3-venv python3-pip -y
+
+# 2. 创建虚拟环境
+python3 -m venv venv
+
+# 3. 激活虚拟环境
+source venv/bin/activate
+
+# 4. 安装依赖（在虚拟环境里）
+pip install flask requests beautifulsoup4 lxml
+
+# 5. 运行你的 server.py
+python server.py
+
+# 测试
+curl "http://localhost:5000/search?wd=robot"
